a96de172fd4ed59016f0460bab8984112f69a8fd
Icy-cui <icercyf@gmail.com>
2023-10-26 16:16:27 +1100

    feat: test bootstrap, add about us

---
+++ pages/about/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function index() {
+  return (
+    <button
+      onClick={() => {
+        console.log("woo~");
+      }}
+    >
+      test page : about us
+    </button>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,29 @@
-import Head from 'next/head'
-import Header from '@components/Header'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>icy&hang homeee</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome Dr. hang!" />
-        <p className="description">
-          Get started sooooon...
-        </p>
+        <p className="description">Get started sooooon...</p>
+        <ul className="menu">
+          <li>
+            <Link href="/about" passHref>
+              <a>about us</a>
+            </Link>
+          </li>
+        </ul>
       </main>
+
+      <Footer></Footer>
     </div>
-  )
+  );
 }
